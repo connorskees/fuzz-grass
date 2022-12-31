51f2ed149cf9d9eade0ffdf2afffefbbb59f3ff7
connorskees <connor1skees@gmail.com>
2022-12-31 04:24:23 +0000

    update, add syntax dictionary

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use afl;
+#[macro_use]
+extern crate afl;
 use grass;
 
 fn main() {
-    afl::fuzz(|data: &[u8]| {
+        fuzz!(|data: &[u8]| {
         if let Ok(s) = std::str::from_utf8(data) {
             let _ = grass::from_string(s.to_string(), &grass::Options::default());                    
         }
